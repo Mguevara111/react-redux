e5e0267a2b866decdfbe6d17e67cb764a9666513
el perro <marcelo.guevara1983@gmail.com>
2026-04-22 13:55:54 -0500

    page 4040 ready

--- src/components/e404.jsx
+++ src/components/e404.jsx
@@ -1,12 +1,12 @@
 import React from "react";
+
 import { Link } from "react-router-dom";
 
 export function E404(){
     return(
         <section className="e404">
-            <h2>pagina 404</h2>
             <Link to='/'>
-                <button>Return</button>
+                <button className="button e404btn">Return</button>
             </Link>
         </section>
     );
