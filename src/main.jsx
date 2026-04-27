7ac55917d4e6fabedbbc123b591258ecc1105bd0
el perro <marcelo.guevara1983@gmail.com>
2026-04-27 14:13:57 -0500

    completado1

--- src/main.jsx
+++ src/main.jsx
@@ -10,7 +10,7 @@ import App from './App.jsx'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
+      <BrowserRouter basename='/react-redux'>
         <App />
       </BrowserRouter>
     </Provider>
